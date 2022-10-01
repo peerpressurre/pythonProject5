36fb1672454d8f3e4fbf82ba81d37cc5903f4805
student vova <popihacvova@gmail.com>
2022-10-01 12:49:26 +0300

    Update: feature/hm_ex2

--- main.py
+++ main.py
@@ -12,3 +12,10 @@ while num1 < num2:
     num1 += 1
 else:
     print(f'number = {num1}')
+
+print('Усі числа діапазону за спаданням')
+while num2 > num1:
+    print(f'{num2}')
+    num2-= 1
+else:
+    print(f'{num2}')
