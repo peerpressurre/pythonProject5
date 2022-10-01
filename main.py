b431fca6c8fc7c045784149d7fc59e762e0a8571
student vova <popihacvova@gmail.com>
2022-10-01 12:45:59 +0300

    Update: feature/hm_ex2

--- main.py
+++ main.py
@@ -1,40 +1,12 @@
 num1 = int(input('Enter first number->'))
 num2 = int(input('Enter second number->'))
+
 #усі числа діапазону
+print('Усі числа діапазону')
+number = num1
+
 while num1 < num2:
     print(f'number = {num1}')
     num1 += 1
 else:
-    print(f'number = {num1})
-#в порядку спадання
-if num1 > num2:
-    temp = num1
-    num1 = num2
-    num2 = temp
-
-while num2 > num1:
-    print(f'{num2}')
-    num2 -= 1
-else:
-    print(f'{num2}')
-#кратне 7
-if num1 < 7:
-    num1 += (7 % num1)
-elif num1 > 7:
-    num1 = (num1 - (num1 % 7))+7
-number = num1
-
-while number <= num2:
-    print(f'{number}')
-    number += 7
-#кратне 5
-if num1 < 5:
-    num1 += (5 % num1)
-elif num1 > 5:
-    num1 = (num1 - (num1 % 5))+5
-number = num1
-
-while number <= num2:
-    print(f'{number}')
-    number += 5
-
+    print(f'number = {num1}')
