fc2efd0bff73df4115e76336f7c3d10b95b8f9c1
student vova <popihacvova@gmail.com>
2022-10-01 13:16:21 +0300

    Update: feature/hm_ex2

---
+++ main.py
@@ -0,0 +1,18 @@
+num1 = int(input('->'))
+num2 = int(input('->'))
+
+while num1 < num2:
+    if num1 % 3 == 0:
+        print('Fizz')
+    elif num1 & 5 == 0:
+        print('Buzz')
+    elif num1 % 3 == 0:
+        if num1 % 5 == 0:
+            print('Fizz Buzz')
+    elif num1 % 3 == 1:
+        if num1 % 5 == 1:
+            print('')
+
+
+
+
